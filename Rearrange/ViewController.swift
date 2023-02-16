bbce965ddb3f20c0750164692d7a30876b850683
SatoshiKawazoe0715 <124865153+SatoshiKawazoe0715@users.noreply.github.com>
2023-02-16 23:58:47 +0900

    Initial Commit

---
+++ Rearrange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rearrange
+//
+//  Created by Satoshi Kawazoe on 2023/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
